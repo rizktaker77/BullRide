8b906da0250a43dba1038b7ecf0aa3767a2c4d36
Paulo Rizkalla <paulo.rizkalla@gmail.com>
2016-10-23 11:59:30 -0700

    Updated unit test run batch command to fit Jenkins job folder paths

--- Batch/run_unit_tests.bat
+++ Batch/run_unit_tests.bat
@@ -1,11 +1,11 @@
 echo "Running unit tests for %CD%"
 
-IF NOT EXIST %CD%\Logs (
-	mkdir %CD%\Logs
+IF NOT EXIST "%CD%\Logs" (
+	mkdir "%CD%\Logs"
 )
 
-IF NOT EXIST %CD%\TestResults (
-	mkdir %CD%\TestResults
+IF NOT EXIST "%CD%\TestResults" (
+	mkdir "%CD%\TestResults"
 )
 
 "C:\Program Files\Unity\Editor\Unity.exe" -nographics -quit -batchmode -projectPath "%CD%" -logFile "%CD%\Logs\%DATE:~-4%-%DATE:~3,2%-%DATE:~0,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%.log" -runEditorTests -editorTestsResultFile "%CD%\TestResults\unity_unittests_results.xml" -editorTestsFilter "Core Game Tests"
